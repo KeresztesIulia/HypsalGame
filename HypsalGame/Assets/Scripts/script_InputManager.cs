61b1e6d1df98788bd2f338c7013b60dcddb55886
Iulia Keresztes <101juliakeresztes@gmail.com>
2026-03-17 05:46:42 +0100

    Set up inputs structure and interaction button

---
+++ HypsalGame/Assets/Scripts/script_InputManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class script_InputManager : MonoBehaviour
+{
+    public static PlayerInput playerInput;
+    public static InputActionAsset inputActions;
+
+    public static InputActionMap map_PlayerMap;
+    public static InputAction action_Interact;
+
+    public static InputActionMap map_uiMap;
+
+    public void Initialize()
+    {
+        playerInput = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerInput>();
+        inputActions = playerInput?.actions;
+
+        map_PlayerMap = inputActions?.FindActionMap("Player");
+        action_Interact = map_PlayerMap?.FindAction("Interact");
+
+        map_uiMap = inputActions?.FindActionMap("UI");
+    }
+}
