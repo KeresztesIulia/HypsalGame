7ca2181d1e90b5d34cb73a62ada1a4a20234803a
Iulia Keresztes <101juliakeresztes@gmail.com>
2026-03-31 07:47:43 +0200

    Combine two similar scripts for initializing objects

--- HypsalGame/Assets/Scripts/script_PersistentInitializer.cs
+++ HypsalGame/Assets/Scripts/script_PersistentInitializer.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 public class script_PersistentInitializer : MonoBehaviour, interface_PersistentData
 {
     [SerializeField] GameObject[] _subModules;
+    [SerializeField] bool _instantiateSubmodules = false;
 
     bool initialized = false;
     public void Initialize()
     {
         foreach (var module in _subModules)
-            foreach (var persistentData in module.GetComponents<Component>())
+        {
+            var finalModule = module;
+            if (_instantiateSubmodules) finalModule = Instantiate(finalModule);
+            foreach (var persistentData in module.GetComponents<MonoBehaviour>())
+            {
+                if (!persistentData.enabled) continue;
                 if (persistentData is interface_PersistentData)
                     (persistentData as interface_PersistentData).Initialize();
+            }
+        }
+                
         initialized = true;
     }
 
