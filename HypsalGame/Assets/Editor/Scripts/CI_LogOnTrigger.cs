d6c8b0d7b09f3c6a31ebf4c58eee1ec5d46665cf
Iulia Keresztes <101juliakeresztes@gmail.com>
2026-04-20 12:43:48 +0200

    Make log triggerable on entering area

---
+++ HypsalGame/Assets/Editor/Scripts/CI_LogOnTrigger.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(script_LogOnTrigger))]
+public class CI_LogOnTrigger : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        using (new EditorGUI.DisabledScope(true))
+        {
+            EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour((MonoBehaviour)target), GetType(), false);
+            EditorGUILayout.ObjectField("Custom Inspector", MonoScript.FromScriptableObject(this), GetType(), false);
+        }
+
+        MakeList();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_triggerOnce"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_popUpLog"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void MakeList()
+    {
+        var textToLogProp = serializedObject.FindProperty("_textsToLog");
+        var logTypesProp = serializedObject.FindProperty("_logTypes");
+
+        EditorGUILayout.LabelField("Texts to log", EditorStyles.boldLabel);
+
+        for (int i = 0; i < textToLogProp.arraySize; i++)
+        {
+            var textProp = textToLogProp.GetArrayElementAtIndex(i);
+            var logTypeProp = logTypesProp.GetArrayElementAtIndex(i);
+
+            EditorGUILayout.BeginHorizontal();
+
+            textProp.stringValue = EditorGUILayout.TextField(textProp.stringValue);
+            logTypeProp.enumValueIndex = EditorGUILayout.Popup("",
+                                                            logTypeProp.enumValueIndex,
+                                                            logTypeProp.enumDisplayNames);
+
+            if (GUILayout.Button("-"))
+            {
+                textToLogProp.DeleteArrayElementAtIndex(i);
+                logTypesProp.DeleteArrayElementAtIndex(i);
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("+"))
+        {
+            textToLogProp.arraySize++;
+            logTypesProp.arraySize++;
+        }
+    }
+}
