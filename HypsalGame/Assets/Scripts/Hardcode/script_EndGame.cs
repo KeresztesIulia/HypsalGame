13d6fecd926752f867d481ccfcb04fcc761b6b95
Iulia Keresztes <101juliakeresztes@gmail.com>
2026-04-28 19:33:31 +0200

    "Help AI" ending elements starting to take shape

--- HypsalGame/Assets/Scripts/Hardcode/script_EndGame.cs
+++ HypsalGame/Assets/Scripts/Hardcode/script_EndGame.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class script_EndGame : MonoBehaviour
@@ -13,11 +15,43 @@ public class script_EndGame : MonoBehaviour
 #endif
     }
 
+    public void Close()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
     public void Delete()
     {
-        string fileToCopy = "sd.bat";
-        string destinationDirectory = "c:\\myDestinationFolder\\";
+        string streamingAssetsPath = Application.streamingAssetsPath;
+        string fileToCopy = Path.Combine(streamingAssetsPath, "sd.bat");
+        string destinationDirectory = Application.persistentDataPath;
+        string copyDestination = Path.Combine(destinationDirectory, Path.GetFileName(fileToCopy));
 
-        File.Copy(fileToCopy, destinationDirectory + Path.GetFileName(fileToCopy));
+        string buildDirectory = Path.GetDirectoryName(Application.dataPath); // send to the batch file
+
+        File.Copy(fileToCopy, copyDestination, true);
+
+        ProcessStartInfo ProcessInfo;
+        Process Process;
+
+        Process process = new Process();
+
+        // redirect the output stream of the child process.
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = false;
+        process.StartInfo.CreateNoWindow = false;
+        process.StartInfo.FileName = copyDestination;
+        //process.StartInfo.Arguments = arguments;
+        //process.StartInfo.
+
+        SubtitleManager.Instance?.ShowSubtitle(SubtitleManager.SubtitleType.Generic, copyDestination, 10);
+
+        process.Start();
+        Application.Quit();
+
     }
 }
