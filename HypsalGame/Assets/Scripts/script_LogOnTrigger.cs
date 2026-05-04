c2f8dae8e47b33d965bc119817d62fbf01528b46
Iulia Keresztes <101juliakeresztes@gmail.com>
2026-05-04 16:06:09 +0200

    logOnTrigger doesn't trigger multiple times now
    Unity is annoying, even when it does make sense.

--- HypsalGame/Assets/Scripts/script_LogOnTrigger.cs
+++ HypsalGame/Assets/Scripts/script_LogOnTrigger.cs
@@ -7,6 +7,8 @@ public class script_LogOnTrigger : MonoBehaviour
     [SerializeField] bool _triggerOnce = true;
     [SerializeField] bool _popUpLog = true;
 
+    bool triggered = false;
+
     public void LogTexts()
     {
         if (_textsToLog == null || _textsToLog.Length == 0) return;
@@ -24,9 +26,9 @@ public class script_LogOnTrigger : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
         if (_textsToLog == null || _textsToLog.Length == 0) return;
+        if (_triggerOnce && triggered) return;
 
         LogTexts();
-
-        if (_triggerOnce) enabled = false;
+        triggered = true;
     }
 }
